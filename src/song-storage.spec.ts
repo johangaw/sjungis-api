8f483b37382b77e50c2c087aa00c4af0694f79ad
Johan Gustavsson <johan.gustavsson@valtech.se>
2019-08-04 17:11:09 +0200

    Added tests and fixed bug
    The bug fix prevents unnecessary updates of the nameUrl field

---
+++ src/song-storage.spec.ts
@@ -0,0 +1,66 @@
+process.env.MONGODB_URL = 'mongodb://localhost:27017/sjungis-test'
+
+import { expect } from 'chai';
+import 'mocha';
+import { all, songsCollection, edit, create } from './song-storage';
+import { MongoClient, Collection, ObjectId } from 'mongodb';
+import { ISong } from './etc';
+
+let col: Collection<ISong>, client: MongoClient;
+
+before(async () => {
+  [col, client] = await songsCollection();
+})
+
+after(async () => {
+  client.close();
+})
+
+beforeEach(() => {
+  return col.deleteMany({});
+})
+
+describe('all', () => {
+  describe('when the db is empty', () => {
+    it('return an empty array', async () => {
+      expect(await all()).to.be.empty;
+    });
+  });
+});
+
+describe('edit', () => {
+  it('does not change the urlName', async () => {
+    const song = {name: 'name', melody: 'mel 1', urlName: 'name', lyrics: 'lyrics', _id: undefined as any}
+    const result = await col.insertOne(song);
+    await edit({...song, melody: 'mel 2'});
+    const updatedSong = await col.findOne({_id: result.insertedId});
+    expect(updatedSong.name).to.equal('name');
+    expect(updatedSong.melody).to.equal('mel 2');
+    expect(updatedSong.lyrics).to.equal('lyrics');
+    expect(updatedSong.urlName).to.equal('name');
+  });
+});
+
+describe('create', () => {
+  it('sets the urlName to the name', async () => {
+    const song = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics'});
+    const created = await col.findOne({_id: new ObjectId(song._id)});
+    expect(created.name).to.equal('name');
+    expect(created.melody).to.equal('mel 1');
+    expect(created.lyrics).to.equal('lyrics');
+    expect(created.urlName).to.equal('name');
+  });
+
+  describe('when creating a second song the the same name', () => {
+    it('invent a nameUrl for the second one', async () => {
+      const song1 = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics'});
+      const created1 = await col.findOne({_id: new ObjectId(song1._id)});
+      expect(created1.urlName).to.equal('name');
+  
+      const song2 = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics'});
+      const created2 = await col.findOne({_id: new ObjectId(song2._id)});
+      expect(created2.urlName).to.not.equal('name');
+      expect(created2.urlName.startsWith('name')).to.be.true;
+    });
+  });
+});
--- src/song-storage.ts
+++ src/song-storage.ts
@@ -1,19 +1,20 @@
 import { ISong, ISongParams } from './etc';
 import { MongoClient, Collection, ObjectId } from 'mongodb';
 
-const MONGO_URL = process.env['MONGODB_URL'] || 'mongodb://localhost:27017'
+const MONGO_URL = process.env['MONGODB_URL'] || 'mongodb://localhost:27017/sjungis'
 
 function getURL(name: string): string {
   return encodeURIComponent(name);
 }
 
 async function createURL(name: string, id?: string): Promise<string> {
-  const col = await songsCollection();
+  const [col, client] = await songsCollection();
   let urlName = getURL(name);
 
   while(true) {
     const results = await col.find({urlName: urlName}).toArray();
-    if(results.length === 0 || results.length === 1 && results[0]._id === id) {
+    if(results.length === 0 || results.length === 1 && new ObjectId(results[0]._id).equals(id)) {
+      client.close();
       return urlName;
     } else {
       urlName = getURL(`${name} ${new Date().getMilliseconds()}`);
@@ -21,27 +22,32 @@ async function createURL(name: string, id?: string): Promise<string> {
   }
 }
 
-async function songsCollection(): Promise<Collection<ISong>> {
+export async function songsCollection(): Promise<[Collection<ISong>, MongoClient]> {
   const client = await MongoClient.connect(MONGO_URL);
-  const db = client.db('sjungis');
+  const db = client.db();
   const collection = db.collection('songs');
-  return collection;
+  return [collection, client];
 }
 
 export async function all(): Promise<ISong[]> {
-  const col = await songsCollection();
-  return col.find().toArray();
+  const [col, client] = await songsCollection();
+  const songs = col.find().toArray();
+  client.close();
+  return songs;
 }
 
 export async function get(urlName: string): Promise<ISong> {
-  const col = await songsCollection();
-  return col.findOne({urlName: getURL(urlName)});
+  const [col, client] = await songsCollection();
+  const song = col.findOne({urlName: getURL(urlName)});
+  client.close();
+  return song;
 }
 
 export async function create(songParams: ISongParams): Promise<ISong> {
-  const col = await songsCollection();
+  const [col, client] = await songsCollection();
   const song: ISong = {...songParams, urlName: await createURL(songParams.name), _id: undefined};
   const result = await col.insertOne(song)
+  client.close();
   if (result.insertedCount === 1) {
     return {...song, _id: result.insertedId.toHexString()}
   } else {
@@ -50,13 +56,14 @@ export async function create(songParams: ISongParams): Promise<ISong> {
 }
 
 export async function edit(song: ISong): Promise<ISong> {
-  const col = await songsCollection();
+  const [col, client] = await songsCollection();
   const {name, lyrics, melody} = song;
   const result = await col.findOneAndUpdate(
     {_id: new ObjectId(song._id)},
     {$set: {name, lyrics, melody, urlName: await createURL(song.name, song._id)}},
     {returnOriginal: false}
   );
+  client.close();
   if (result.value) {
     return result.value;
   } else {
