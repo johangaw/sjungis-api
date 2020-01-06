c494128ae9b91a408031f4a90f36e16af7135d72
Johan Gustavsson <johan.gustavsson@valtech.se>
2020-01-06 12:05:49 +0100

    Fixed brooken tests

--- src/song-storage.spec.ts
+++ src/song-storage.spec.ts
@@ -30,34 +30,36 @@ describe('all', () => {
 
 describe('edit', () => {
   it('does not change the urlName', async () => {
-    const song = {name: 'name', melody: 'mel 1', urlName: 'name', lyrics: 'lyrics', _id: undefined as any}
+    const song = {name: 'name', melody: 'mel 1', urlName: 'name', lyrics: 'lyrics', obscene: true, _id: undefined as any}
     const result = await col.insertOne(song);
-    await edit({...song, melody: 'mel 2'});
+    await edit({...song, melody: 'mel 2', obscene: false});
     const updatedSong = await col.findOne({_id: result.insertedId});
     expect(updatedSong.name).to.equal('name');
     expect(updatedSong.melody).to.equal('mel 2');
     expect(updatedSong.lyrics).to.equal('lyrics');
     expect(updatedSong.urlName).to.equal('name');
+    expect(updatedSong.obscene).to.equal(false);
   });
 });
 
 describe('create', () => {
   it('sets the urlName to the name', async () => {
-    const song = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics'});
+    const song = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics', obscene: true});
     const created = await col.findOne({_id: new ObjectId(song._id)});
     expect(created.name).to.equal('name');
     expect(created.melody).to.equal('mel 1');
     expect(created.lyrics).to.equal('lyrics');
     expect(created.urlName).to.equal('name');
+    expect(created.obscene).to.equal(true);
   });
 
   describe('when creating a second song the the same name', () => {
     it('invent a nameUrl for the second one', async () => {
-      const song1 = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics'});
+      const song1 = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics', obscene: false});
       const created1 = await col.findOne({_id: new ObjectId(song1._id)});
       expect(created1.urlName).to.equal('name');
   
-      const song2 = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics'});
+      const song2 = await create({name: 'name', melody: 'mel 1', lyrics: 'lyrics', obscene: false});
       const created2 = await col.findOne({_id: new ObjectId(song2._id)});
       expect(created2.urlName).to.not.equal('name');
       expect(created2.urlName.startsWith('name')).to.be.true;
